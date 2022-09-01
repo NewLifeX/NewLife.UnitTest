00ee12e72da177d5433fa21f525ddcba5d160d01
大石头 <nnhy@vip.qq.com>
2022-09-01 11:44:06 +0800

    v1.0.2022.0901 优化单元测试的顺序日志输出

--- NewLife.UnitTest/DefaultOrderer.cs
+++ NewLife.UnitTest/DefaultOrderer.cs
@@ -5,91 +5,104 @@ using NewLife.Log;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
-namespace NewLife.UnitTest
+namespace NewLife.UnitTest;
+
+/// <summary>默认顺序测试排序器</summary>
+/// <remarks>
+/// 默认排序（按方法在类内出现的顺序）：
+/// [TestCaseOrderer("NewLife.UnitTest.DefaultOrderer", "NewLife.UnitTest")]
+/// </remarks>
+public class DefaultOrderer : ITestCaseOrderer
 {
-    /// <summary>默认顺序测试排序器</summary>
-    /// <remarks>
-    /// 默认排序（按方法在类内出现的顺序）：
-    /// [TestCaseOrderer("NewLife.UnitTest.DefaultOrderer", "NewLife.UnitTest")]
-    /// </remarks>
-    public class DefaultOrderer : ITestCaseOrderer
+    private readonly IMessageSink _messageSink;
+
+    /// <summary>实例化默认顺序测试排序器</summary>
+    /// <param name="messageSink"></param>
+    public DefaultOrderer(IMessageSink messageSink) => _messageSink = messageSink;
+
+    /// <summary>对测试用例进行排序</summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="testCases"></param>
+    /// <returns></returns>
+    public IEnumerable<T> OrderTestCases<T>(IEnumerable<T> testCases) where T : ITestCase
     {
-        private readonly IMessageSink _messageSink;
+        // 所有测试用例
+        var ts = testCases.ToList();
+        //var dic2 = new SortedDictionary<Int32, List<T>>();
 
-        /// <summary>实例化默认顺序测试排序器</summary>
-        /// <param name="messageSink"></param>
-        public DefaultOrderer(IMessageSink messageSink) => _messageSink = messageSink;
-
-        /// <summary>对测试用例进行排序</summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="testCases"></param>
-        /// <returns></returns>
-        public IEnumerable<T> OrderTestCases<T>(IEnumerable<T> testCases) where T : ITestCase
+        // 借助反射，拿到方法列表，此时为方法在源码中出现顺序。
+        // 需要注意，方法对象不唯一，需要拼接名称字符串
+        var types = new List<String>();
+        var methods = new List<String>();
+        foreach (var item in ts)
         {
-            // 所有测试用例
-            var ts = testCases.ToList();
-            var dic2 = new SortedDictionary<Int32, List<T>>();
+            var cls = item.TestMethod?.TestClass?.Class;
+            if (cls != null && !types.Contains(cls.Name))
+            {
+                types.Add(cls.Name);
+                foreach (var elm in cls.GetMethods(false))
+                {
+                    var key = $"{cls.Name}-{elm.Name}";
+                    if (!methods.Contains(key)) methods.Add(key);
+                }
+            }
+        }
 
-            // 借助反射，拿到方法列表，此时为方法在源码中出现顺序。
-            // 需要注意，方法对象不唯一，需要拼接名称字符串
-            var types = new List<String>();
-            var methods = new List<String>();
+        XTrace.WriteLine("使用[DefaultOrderer/默认顺序]测试: {0}, 用例：{1}", types.Join(), ts.Count);
+
+        var dic = new SortedDictionary<Int32, List<T>>();
+
+        // 从源码行获取优先级
+        foreach (var testCase in ts)
+        {
+            var priority = testCase.SourceInformation?.LineNumber ?? 0;
+            if (!dic.TryGetValue(priority, out var list)) list = dic[priority] = new List<T>();
+
+            list.Add(testCase);
+        }
+
+        // 遇到无效源码信息时，从方法出现顺序为准
+        if (dic.Count == 1 && dic.First().Key == 0)
+        {
+            // 按照方法顺序，逐个返回测试用例，并从列表中删除
+            foreach (var method in methods)
+            {
+                for (var i = 0; i < ts.Count; i++)
+                {
+                    var item = ts[i];
+                    var key = $"{item.TestMethod.TestClass.Class.Name}-{item.TestMethod.Method.Name}";
+                    if (method == key)
+                    {
+                        XTrace.WriteLine(key);
+
+                        yield return item;
+                        ts.RemoveAt(i);
+                        i--;
+
+                        break;
+                    }
+                }
+            }
+
+            // 如果列表中还有测试用例，可能上述逻辑有缺陷，仍然返回
             foreach (var item in ts)
             {
-                var cls = item.TestMethod?.TestClass?.Class;
-                if (cls != null && !types.Contains(cls.Name))
-                {
-                    types.Add(cls.Name);
-                    foreach (var elm in cls.GetMethods(false))
-                    {
-                        var key = $"{cls.Name}-{elm.Name}";
-                        if (!methods.Contains(key)) methods.Add(key);
-                    }
-                }
+                var key = $"{item.TestMethod.TestClass.Class.Name}-{item.TestMethod.Method.Name}";
+                XTrace.WriteLine(key);
+
+                yield return item;
             }
 
-            XTrace.WriteLine("使用[DefaultOrderer/默认顺序]测试: {0}, 用例：{1}", types.Join(), ts.Count);
+            yield break;
+        }
 
-            var dic = new SortedDictionary<Int32, List<T>>();
-
-            // 从源码行获取优先级
-            foreach (var testCase in ts)
+        foreach (var item in dic)
+        {
+            foreach (var testCase in item.Value.OrderBy(x => x.TestMethod.Method.Name))
             {
-                var priority = testCase.SourceInformation?.LineNumber ?? 0;
-                if (!dic.TryGetValue(priority, out var list)) list = dic[priority] = new List<T>();
+                var key = $"{item.Key}-{testCase.TestMethod.TestClass.Class.Name}-{testCase.TestMethod.Method.Name}";
+                XTrace.WriteLine(key);
 
-                list.Add(testCase);
-            }
-
-            // 遇到无效源码信息时，从以方法出现顺序为准
-            if (dic.Count == 1 && dic.First().Key == 0)
-            {
-                // 按照方法顺序，逐个返回测试用例，并从列表中删除
-                foreach (var method in methods)
-                {
-                    for (var i = 0; i < ts.Count; i++)
-                    {
-                        var item = ts[i];
-                        var key = $"{item.TestMethod.TestClass.Class.Name}-{item.TestMethod.Method.Name}";
-                        if (method == key)
-                        {
-                            yield return item;
-                            ts.RemoveAt(i);
-                        }
-                    }
-                }
-
-                // 如果列表中还有测试用例，可能上述逻辑有缺陷，仍然返回
-                foreach (var item in ts)
-                {
-                    yield return item;
-                }
-
-                yield break;
-            }
-
-            foreach (var testCase in dic.SelectMany(e => e.Value.OrderBy(x => x.TestMethod.Method.Name)))
-            {
                 yield return testCase;
             }
         }
--- NewLife.UnitTest/PriorityOrderer.cs
+++ NewLife.UnitTest/PriorityOrderer.cs
@@ -5,58 +5,67 @@ using NewLife.Log;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
-namespace NewLife.UnitTest
+namespace NewLife.UnitTest;
+
+/// <summary>优先级顺序测试排序器</summary>
+/// <remarks>
+/// 提供测试用例排序支持：
+/// [TestCaseOrderer("NewLife.UnitTest.PriorityOrderer", "NewLife.UnitTest")]
+/// </remarks>
+public class PriorityOrderer : ITestCaseOrderer
 {
-    /// <summary>优先级顺序测试排序器</summary>
-    /// <remarks>
-    /// 提供测试用例排序支持：
-    /// [TestCaseOrderer("NewLife.UnitTest.PriorityOrderer", "NewLife.UnitTest")]
-    /// </remarks>
-    public class PriorityOrderer : ITestCaseOrderer
+    /// <summary>对测试用例进行排序</summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="testCases"></param>
+    /// <returns></returns>
+    public IEnumerable<T> OrderTestCases<T>(IEnumerable<T> testCases) where T : ITestCase
     {
-        /// <summary>对测试用例进行排序</summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="testCases"></param>
-        /// <returns></returns>
-        public IEnumerable<T> OrderTestCases<T>(IEnumerable<T> testCases) where T : ITestCase
+        // 所有测试用例
+        var ts = testCases.ToList();
+        var dic2 = new SortedDictionary<Int32, List<T>>();
+
+        // 借助反射，拿到方法列表，此时为方法在源码中出现顺序。
+        // 需要注意，方法对象不唯一，需要拼接名称字符串
+        var types = new List<String>();
+        foreach (var item in ts)
         {
-            // 所有测试用例
-            var ts = testCases.ToList();
-            var dic2 = new SortedDictionary<Int32, List<T>>();
-
-            // 借助反射，拿到方法列表，此时为方法在源码中出现顺序。
-            // 需要注意，方法对象不唯一，需要拼接名称字符串
-            var types = new List<String>();
-            foreach (var item in ts)
+            var cls = item.TestMethod?.TestClass?.Class;
+            if (cls != null && !types.Contains(cls.Name))
             {
-                var cls = item.TestMethod?.TestClass?.Class;
-                if (cls != null && !types.Contains(cls.Name))
-                {
-                    types.Add(cls.Name);
-                }
+                types.Add(cls.Name);
+            }
+        }
+
+        XTrace.WriteLine("使用[PriorityOrderer/优先级顺序]测试: {0}, 用例：{1}", types.Join(), ts.Count);
+
+        var assemblyName = typeof(TestOrderAttribute).AssemblyQualifiedName!;
+        var dic = new SortedDictionary<Int32, List<T>>();
+        foreach (var testCase in testCases)
+        {
+            var priority = 0;
+            var atts = testCase.TestMethod.Method.GetCustomAttributes(assemblyName).ToList();
+            foreach (var att in atts)
+            {
+                var n = att.GetNamedArgument<Int32>(nameof(TestOrderAttribute.Priority));
+                if (n != 0) priority = n;
             }
 
-            XTrace.WriteLine("使用[PriorityOrderer/优先级顺序]测试: {0}, 用例：{1}", types.Join(), ts.Count);
+            if (!dic.TryGetValue(priority, out var list)) list = dic[priority] = new List<T>();
 
-            var assemblyName = typeof(TestOrderAttribute).AssemblyQualifiedName!;
-            var dic = new SortedDictionary<Int32, List<T>>();
-            foreach (var testCase in testCases)
+            list.Add(testCase);
+        }
+
+        //foreach (var testCase in dic.SelectMany(e => e.Value.OrderBy(x => x.TestMethod.Method.Name)))
+        //{
+        //    yield return testCase;
+        //}
+        foreach (var item in dic)
+        {
+            foreach (var testCase in item.Value.OrderBy(x => x.TestMethod.Method.Name))
             {
-                var priority = 0;
-                var atts = testCase.TestMethod.Method.GetCustomAttributes(assemblyName).ToList();
-                foreach (var att in atts)
-                {
-                    var n = att.GetNamedArgument<Int32>(nameof(TestOrderAttribute.Priority));
-                    if (n != 0) priority = n;
-                }
+                var key = $"{item.Key}-{testCase.TestMethod.TestClass.Class.Name}-{testCase.TestMethod.Method.Name}";
+                XTrace.WriteLine(key);
 
-                if (!dic.TryGetValue(priority, out var list)) list = dic[priority] = new List<T>();
-
-                list.Add(testCase);
-            }
-
-            foreach (var testCase in dic.SelectMany(e => e.Value.OrderBy(x => x.TestMethod.Method.Name)))
-            {
                 yield return testCase;
             }
         }
